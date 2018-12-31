62a070c4b6b5cb2b201a23fb22ffaf9ccd7a36b1
mkrul <mishakrul@gmail.com>
2018-12-31 15:08:30 -0500

    map out all contests

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,28 @@
 import React from "react";
 import Header from "./Header";
+import ContestPreview from "./ContestPreview";
 
 class App extends React.Component {
 	state = {
 		pageHeader: "Naming Contests"
 	};
 
+	componentDidMount() {
+
+	}
+
+	componentWillUnmount() {
+
+	}
+
 	render() {
 		return (
 			<div className="App">
-				<Header message={ this.state.pageHeader }/>
+				<Header message={this.state.pageHeader} />
 				<div>
-					{ this.state.test }
+					{this.props.contests.map(contest =>
+						<ContestPreview {...contest} />
+					)}
 				</div>
 			</div>
 		);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import data from "./testData";
 import App from "./components/App";
 
-console.log(data);
-
 ReactDOM.render(
 	<App contests={ data.contests }/>,
 	document.getElementById("root")
