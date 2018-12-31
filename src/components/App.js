a3d5561965ca0c311066e21b049d0f26cc63f30c
mkrul <mishakrul@gmail.com>
2018-12-31 15:29:22 -0500

    add testData to state

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import ContestPreview from "./ContestPreview";
+import data from "../testData";
 
 class App extends React.Component {
 	state = {
-		pageHeader: "Naming Contests"
+		pageHeader: "Naming Contests",
+		contests: []
 	};
 
 	componentDidMount() {
-		//
+		this.setState({
+			contests: data.contests
+		});
 	}
 	componentWillUnmount() {
 		//
@@ -19,8 +23,8 @@ class App extends React.Component {
 			<div className="App">
 				<Header message={this.state.pageHeader} />
 				<div>
-					{this.props.contests.map(contest =>
-						<ContestPreview {...contest} />
+					{this.state.contests.map(contest =>
+						<ContestPreview key={contest.id} {...contest} />
 					)}
 				</div>
 			</div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import data from "./testData";
 import App from "./components/App";
 
 ReactDOM.render(
-	<App contests={ data.contests }/>,
+	<App />,
 	document.getElementById("root")
 );
